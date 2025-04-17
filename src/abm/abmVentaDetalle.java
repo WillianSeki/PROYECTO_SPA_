7b6c5e9d0df5e1e7ff8b31910acfc39e3b73cdb2
Codbalto <satelenovo@gmail.com>
2025-04-17 11:45:12 -0400

    Le agregué unas cuantas cosas al código

---
+++ src/abm/abmVentaDetalle.java
@@ -0,0 +1,49 @@
+package abm;
+
+import config.conexion;
+import config.sesion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import modelo.modeloVentaDetalle;
+
+public class abmVentaDetalle extends conexion{
+        sesion oSesion;
+    public abmVentaDetalle(sesion pSesion){
+        oSesion = pSesion;
+    }
+    
+    public boolean cargarDetalle(modeloVentaDetalle pModelo){
+        PreparedStatement preparaConsulta = null;
+        Connection conex = getAbrirConexion();
+        String sql = "";
+        ResultSet resultado = null;
+        
+            try {
+                //SQL PARA VERIFICAR SI HAY UNA FACTURA CON ESE NÚMERO
+                sql = "select * from venta_detalle";
+                preparaConsulta = conex.prepareStatement(sql);
+                preparaConsulta.setInt(1, pModelo.getId_detalle());
+                resultado = preparaConsulta.executeQuery();
+
+            if (resultado.next() == false) {
+                //se carga en el modelo los datos obtenidos de la db-----------------------------------
+                sql = "INSERT INTO venta_detalle(precio, id_venta_cf, id_servicio_cf) values(?,?,?)";
+                preparaConsulta = conex.prepareStatement(sql);
+                preparaConsulta.setInt(1, pModelo.getPrecio());
+                preparaConsulta.setInt(2, pModelo.getId_venta_cf());
+                preparaConsulta.setInt(3, pModelo.getId_servicio_cf());
+                preparaConsulta.execute();
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error: " + e, oSesion.getTituloMensaje(), 1);
+            return false;
+        }
+    }
+}
---
+++ src/modelo/modeloVentaDetalle.java
@@ -0,0 +1,39 @@
+package modelo;
+
+public class modeloVentaDetalle {
+    private int id_detalle, precio, id_venta_cf, id_servicio_cf;
+
+    public int getId_detalle() {
+        return id_detalle;
+    }
+
+    public void setId_detalle(int id_detalle) {
+        this.id_detalle = id_detalle;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getId_venta_cf() {
+        return id_venta_cf;
+    }
+
+    public void setId_venta_cf(int id_venta_cf) {
+        this.id_venta_cf = id_venta_cf;
+    }
+
+    public int getId_servicio_cf() {
+        return id_servicio_cf;
+    }
+
+    public void setId_servicio_cf(int id_servicio_cf) {
+        this.id_servicio_cf = id_servicio_cf;
+    }
+    
+    
+}
